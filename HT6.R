67e224e7f6fed776bfa8e2baf7fb8627630898dc
Gustavo de Leon <gdeleontobias@gmail.com>
2020-04-14 17:55:52 -0600

    trabajando en el test del modelo

--- HT6.R
+++ HT6.R
@@ -1,23 +1,27 @@
 #library(e1071)
 #install.packages("dummy) Esto es para convertir todas las variables categóricas del set de datos
 #install.packages("dummies") Este es para convertir solo una variable
+
 library(caret)
 library(dummies)
 library(plyr)
 library(dplyr)
+
 #Modelo de Regresión logística
+
 setwd("C:/Users/Gustavo/Desktop/SEPTIMO SEMESTRE/MINERIA/HDT6/Hoja-de-Trabajo-06")
 #setwd("C:/Users/alber/Documents/UVG/Septimo semestre/Mineria de Datos/Hoja-Trabajo-5/Hoja-de-trabajo-5")
+
 porcentaje<-0.7
-#datos<-iris
-#set.seed(123)
 set.seed(123)
 datos <- read.csv("train.csv", stringsAsFactors = FALSE)
 
 trainImportantes <- datos[c("MSSubClass","LotFrontage","LotArea","OverallCond","YearBuilt","YearRemodAdd","X2ndFlrSF","FullBath","TotRmsAbvGrd","GarageCars","SalePrice")]
 trainImportantes[is.na(trainImportantes)]<-0
+
 km<-kmeans(trainImportantes,3)
 trainImportantes$grupo<-km$cluster
+
 g1<- trainImportantes[trainImportantes$grupo==1,]
 g2<- trainImportantes[trainImportantes$grupo==2,]
 g3<- trainImportantes[trainImportantes$grupo==3,]
@@ -28,26 +32,20 @@ colnames(trainImportantes)[13] <- "EsBarata"
 colnames(trainImportantes)[14] <- "EsCara"
 colnames(trainImportantes)[15] <- "EsIntermedia"
 
-
 porcentaje<-0.7
 corte <- sample(nrow(trainImportantes),nrow(trainImportantes)*porcentaje)
 train<-trainImportantes[corte,]
 test<-trainImportantes[-corte,]
 
-#corte <- sample(nrow(datos),nrow(datos)*porcentaje)
-#train<-datos[corte,]
-#test<-datos[-corte,]
-
-
-#Queremos saber si una planta es virginica o no
-modelo<-glm(datosvirginica~., data = train[,c(1:4,8)],family = binomial(), maxit=100)
+#Queremos saber si una casa es cara o no
+modelo<-glm(train$trainImportantesCaro~., data = train[,c(1:10,15)],family = binomial(), maxit=100)
 
 #-------------------------------------------------
 # Regresión Logistica 
 #-------------------------------------------------
 
 ##Modelo con todas las variables
-pred<-predict(modelo,newdata = test[,1:4], type = "response")
+pred<-predict(modelo,newdata = test[,1:10], type = "response")
 prediccion<-ifelse(pred>=0.5,1,0)
 confusionMatrix(as.factor(test$datosvirginica),as.factor(prediccion))
 
